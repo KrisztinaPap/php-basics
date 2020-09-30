b39e7552bc3ece71480ffdbaa7cfcde4f4a1c561
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-30 10:23:52 -0600

    Add variable assignment; echo it out;

--- hello-world.php
+++ hello-world.php
@@ -1,5 +1,3 @@
-
-
 <?php // All PHP code must be inside <?php tags!
 // Echo and print are functions we can use to output strings as a response.
 # This is also a single line comment
@@ -9,5 +7,9 @@
     multi-line
     comment */
     echo "Hello World!";
+
+    $studentName = "\nFahad";
+    echo $studentName;
+   
 ?>
 
