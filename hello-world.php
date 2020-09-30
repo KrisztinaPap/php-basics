ca113f81c67eb0422ea4deee3574610cbdd80de3
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-30 10:16:11 -0600

    All about commenting in php;

--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,13 @@
-<?php
+
+
+<?php // All PHP code must be inside <?php tags!
+// Echo and print are functions we can use to output strings as a response.
+# This is also a single line comment
+/*  This 
+    is
+    a
+    multi-line
+    comment */
     echo "Hello World!";
 ?>
+
